991de47340459e985bdd24620450d47530625aab
ณัฐพร นาคพงษ์ <Missterdonat33@gmail.com>
2023-12-07 15:18:18 +0700

    EX: Built-in Array Functions 1

--- ex-1.js
+++ ex-1.js
@@ -2,8 +2,7 @@ let words = ["hello", "world"];
 
 function getWordLengths(words) {
   // Start coding here
+  return words.map((word) => (word.split("")).length)
 }
-
 const result = getWordLengths(words);
-
 console.log(result); // Output: [5, 5]
